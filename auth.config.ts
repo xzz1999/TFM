36e64ffae4f632ef70107ba50c2a71201cae4975
unknown <xiangzhou2013@gmail.com>
2024-05-24 19:37:01 +0200

    prueba

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
 
- 
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -8,27 +7,30 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      //console.log("isLoggedIn:", isLoggedIn);
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       const isOnChat = nextUrl.pathname.startsWith('/chats');
       const isOnPage = nextUrl.pathname.startsWith('/pages');
-      if(isOnChat){
+
+      // Permitir acceso a chats y páginas independientemente de la autenticación
+      if (isOnChat || isOnPage) {
         return true;
       }
-      if(isOnPage){
-        return true;
-      }
-    
+
+      // Si está en dashboard y está autenticado, permitir acceso
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // redirigir a los usuarios a la pagina de login
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-       // return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/login', nextUrl));
       }
+
+      // Si está autenticado y no está en una ruta protegida, redirigir a dashboard
+      if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+
+      // Si no está autenticado y no está en una ruta protegida, permitir acceso
       return true;
     },
   },
-  providers: [], 
+  providers: [],
   secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCxamsg=",
 } satisfies NextAuthConfig;
