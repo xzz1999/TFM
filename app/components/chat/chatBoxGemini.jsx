7996d68b80893e610394a015f6e2b318197b4af7
unknown <xiangzhou2013@gmail.com>
2024-06-20 11:39:36 +0200

    update openAI realizado

--- app/components/chat/chatBoxGemini.jsx
+++ app/components/chat/chatBoxGemini.jsx
@@ -137,7 +137,7 @@ export default function ChatBarGemini() {
                             setMessages(prevMessages => [...prevMessages, { text: response.data, sender: "bot" }]); 
                             await setConversation(interaccion);
                         }else{
-                            interaccion.answer = "lo siento, no puedo responderte acerca de esta tema";
+                            interaccion.answer = "lo siento, no puedo responderte acerca de este tema";
                             interaccion.responseTime = response.time;
                             setMessages(prevMessages => [...prevMessages, { text: "lo siento, no puedo responderte acerca de esta temas", sender: "bot" }]);  
                             await setConversation(interaccion);
